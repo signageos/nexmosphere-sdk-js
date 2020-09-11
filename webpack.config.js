61e44ade4f7e5cb82cb21a1e070dc63574e4a51d
Enchanted57 <4339940-Enchanted57@users.noreply.gitlab.com>
2020-09-11 09:19:24 +0000

    prepare dev environment, identical to front-applet project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+
+const webpack = require('webpack');
+const path = require('path');
+const parameters = require('./config/parameters');
+
+const packageConfigPath = parameters.paths.rootPath + '/package.json';
+
+module.exports = {
+	entry: ['@babel/polyfill', './src/bundle'],
+	output: {
+		path: parameters.paths.distPath,
+		filename: 'bundle.js',
+		publicPath: "/",
+		libraryTarget: 'umd',
+	},
+	devtool: 'source-map',
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': '"' + parameters.environment + '"',
+		}),
+		new webpack.WatchIgnorePlugin([path.resolve(packageConfigPath)]),
+	],
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json'],
+	},
+	module: {
+		rules: [
+			{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
+			{
+				test: /\.(t|j)sx?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: { presets: [require.resolve('@babel/preset-env')] },
+				enforce: 'post',
+			},
+		],
+	},
+}
